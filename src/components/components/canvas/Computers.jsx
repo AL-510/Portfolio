84ba25c295de23d58b3e786be9673508b061d426
AL-510 <114086161+AL-510@users.noreply.github.com>
2024-04-06 13:53:18 +0530

    Update Computers.jsx

--- src/components/components/canvas/Computers.jsx
+++ src/components/components/canvas/Computers.jsx
@@ -21,7 +21,7 @@ const Computers = ({isMobile}) => {
   return (
     <>
     <mesh>
-      <hemisphereLight intensity={2.55} groundColor="black"/>
+      <hemisphereLight intensity={1.55} groundColor="black"/>
       <pointLight intensity={1}/>
       <spotLight position={[-20, 50, 10]}
       angle={0.12}
